bc47c84dcb4e266f007bd462265578c9994dcb31
huhongyun <xiaomiwujiecao@gmail.com>
2019-02-14 00:05:50 +0800

    init

---
+++ example/data_types.dart
@@ -0,0 +1,14 @@
+
+
+
+/* data types
+Numbers
+  Integer
+  Double
+Strings
+Booleans
+Lists
+dynamic
+Maps
+
+*/
---
+++ example/object_orientation.dart
@@ -0,0 +1,9 @@
+class TestClass {
+  void disp() {
+    print("Hello test class");
+  }
+}
+void main() {
+  TestClass c = new TestClass();
+  c.disp();
+}
