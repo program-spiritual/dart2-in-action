e9ead12d2229d7fc62dd0c9fd4de0ad4d9031385
huhongyun <xiaomiwujiecao@gmail.com>
2019-02-14 09:18:16 +0800

    add example

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 ## dart2-in-action
 
 1. [hello world](./bin/dart2-in-action.dart)
-
 2. [object-oriented programming](./example/object_orientation.dart)
-
 3. [data types](./example/data_types.dart)
+4. [operator](./example/operator.dart)
+5. [variables](./example/variables.dart)
---
+++ example/variables.dart
@@ -0,0 +1,20 @@
+//Dart uses the var keyword to achieve the same
+
+var name = "Tom";
+
+//type-checking
+String name2 = 'Smith';
+int num = 10;
+//result in a warning
+//String name = 1;
+//Dart considers all values as objects
+int num2;
+void main(){
+print(num2);
+dynamic x = "tom";
+print(x);
+//Dart throws an exception if an attempt is made to modify variables declared with the final or const keyword.
+final v1 = 12;
+const v2 = 13;
+v2 = 12; // Error: Setter not found: 'v2'.
+}
