319daf4c23433f717d6cf4da1bcfd7fc76dc9b51
huhongyun <xiaomiwujiecao@gmail.com>
2019-02-15 20:27:05 +0800

    symbol

--- example/FooSymbol.dart
+++ example/FooSymbol.dart
@@ -53,4 +53,9 @@ main() {
 现在让我们考虑在类中显示实例方法的数量。预定义类ClassMirror帮助我们实现相同的目标。
   * */
   reflect_InstanceMethods(lib, clsToSearch);
+//  使用  MirrorSystem  类将存储在符号中的类或类的名称转换回字符串。以下代码显示如何将符号转换为字符串。
+  String name_of_lib = MirrorSystem.getName(lib);
+
+  print(lib);
+  print(name_of_lib);
 }
