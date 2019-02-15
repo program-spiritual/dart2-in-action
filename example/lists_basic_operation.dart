f8fff1af84d58ed9a1cb48da9d70e8459a6b472d
huhongyun <xiaomiwujiecao@gmail.com>
2019-02-15 19:55:58 +0800

    lists_basic_operation.dart

--- example/lists_basic_operation.dart
+++ example/lists_basic_operation.dart
@@ -36,4 +36,54 @@ void main() {
 
   l.insertAll(0,[120,130]);
   print(l);
+
+  /*
+  * dart允许修改List中项目的值。换句话说，可以重写列表项的值。以下示例说明了相同的情况 -
+  * */
+  l[0] = 123;
+  print(l);
+  /*
+  * dart：core库中的List类提供了  replaceRange() 函数来修改List项。此函数替换指定范围内元素的值。
+
+使用  List.replaceRange() 函数的语法如下 -
+List.replaceRange(int start_index,int end_index,Iterable <items>)
+Start_index - 表示要开始替换的索引位置的整数。
+
+End_index - 表示要停止替换的索引位置的整数。
+
+<items> - 表示更新值的可迭代对象。
+  * */
+  List l2 = [1, 2, 3,4,5,6,7,8,9];
+  print('The value of list before replacing ${l2}');
+  l2.replaceRange(0,3,[11,23,24]);
+  print('''The value of list after replacing the items 
+    between the range [0-3] is ${l2}''');
+
+  /*
+  *  List.remove() 函数删除列表中第一次出现的指定项。如果从列表中删除指定的值，则此函数返回true。
+  *  value - 表示应从列表中删除的项的值。
+  * */
+
+  bool res = l2.remove(1);
+  print('The value of list after removing the list element ${l2}');
+
+  /*
+  * List.removeAt() 函数删除指定索引处的值并将其返回。
+  * index - 表示应从列表中删除的元素的索引。
+  * */
+  dynamic res2 = l2.removeAt(1);
+  print('The value of the element ${res2}');
+
+  /*
+  *   该List.removeLast() 相同的语法如下 -
+  * */
+  dynamic res3 = l2.removeLast();
+  print('The value of item popped ${res3}');
+
+  /*
+  *   所述List.removeRange()  函数删除指定范围内的物品。相同的语法如下 -
+  * */
+  l2.removeRange(0,3);
+  print('''The value of list after removing the list 
+    element between the range 0-3 ${l2}''');
 }
